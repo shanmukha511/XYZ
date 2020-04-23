27eb8118c9780b9c547ce0b3e8d663fd6400439c
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2020-04-23 22:11:18 +0530

    Update CSV_TO_EXCEL.sh

--- ayehu/automation/google-cloud-audit-automation/dev/CSV_TO_EXCEL.sh
+++ ayehu/automation/google-cloud-audit-automation/dev/CSV_TO_EXCEL.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd tmp/workspace/bera/ayehu/automation/google-cloud-audit-automation/dev/
+cd /tmp/workspace/bera/ayehu/automation/google-cloud-audit-automation/dev/
 for f in *.csv; do 
 mv -- "$f" "${f%.csv}.xlsx"
 done
