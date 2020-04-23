96a9fe7f0b9c62ca7175574fcead529723e6dee0
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2020-04-23 21:23:10 +0530

    Update Optimized_code.ps1

--- ayehu/automation/google-cloud-audit-automation/dev/Optimized_code.ps1
+++ ayehu/automation/google-cloud-audit-automation/dev/Optimized_code.ps1
@@ -51,8 +51,8 @@ else
  echo "Directory already exists"
 }
 
-$Newfilepath = $NewPath + "\" +"DEV Non Production.csv"
-$oldfilepath = $OldPath + "\" +"DEV Non Production.csv"
+$Newfilepath = $NewPath + "\" +"DEV_Non_Production.csv"
+$oldfilepath = $OldPath + "\" +"DEV_Non_Production.csv"
 
 
 
@@ -92,7 +92,7 @@ $data = Get-Instances-Data
 
 $data = $data |Select-Object -SkipLast 1
 
-$path2 = $ENV:WORKSPACE + "\ayehu\automation\google-cloud-audit-automation\dev\DEV Non Production.csv"
+$path2 = $ENV:WORKSPACE + "\ayehu\automation\google-cloud-audit-automation\dev\DEV_Non_Production.csv"
 
 
 
@@ -298,12 +298,12 @@ Write-Verbose "Looking for $server"
 #$results | Out-GridView
 
 $updatetxtpath = $updatepath + "\" + "updated_DEV-nonproduction.txt" 
-$updatecsvpath = $updatepath + "\" + "DEV Non Production Updated.csv"  
+$updatecsvpath = $updatepath + "\" + "DEV_Non_Production_Updated.csv"  
 
 
 
 
-$path = $ENV:WORKSPACE + "\ayehu\automation\google-cloud-audit-automation\dev\DEV Non Production Updated.csv"
+$path = $ENV:WORKSPACE + "\ayehu\automation\google-cloud-audit-automation\dev\DEV_Non_Production_Updated.csv"
 
 $results | Export-Csv -Path $path  -NoTypeInformation
 
