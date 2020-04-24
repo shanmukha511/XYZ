2885c7f20a71e5575af02f0605f17d4de01b2d7b
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2020-04-24 16:28:12 +0530

    Update Csv_To_Xlsx.py

--- ayehu/automation/google-cloud-audit-automation/dev/Csv_To_Xlsx.py
+++ ayehu/automation/google-cloud-audit-automation/dev/Csv_To_Xlsx.py
@@ -1,15 +1,28 @@
-import csv, os
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import csv
+import os
 from glob import glob
 from xlsxwriter.workbook import Workbook
 for csvfile in glob('./*.csv'):
-  name = os.path.basename(csvfile).split('.')[-2]
-workbook = Workbook('./' + str(name) + '.xlsx', {'strings_to_numbers': True,
-'constant_memory': True})
-worksheet = workbook.add_worksheet()
-with open(csvfile, 'r') as f:
- r = csv.reader(f)
- for row_index, row in enumerate(r):
-   for col_index, data in enumerate(row):
-     worksheet.write(row_index, col_index, data)
-     worksheet.set_column(0, col_index, 100)
-workbook.close()
+    name = os.path.basename(csvfile).split('.')[-2]
+    print name
+    print csvfile
+    workbook = Workbook('./' + str(name) + '.xlsx',
+                    {'strings_to_numbers': True,
+                    'constant_memory': True})
+    print "hi"
+    print workbook
+    worksheet = workbook.add_worksheet()
+    print "bi"
+    print worksheet
+    print "mi"
+    print csvfile
+    with open(csvfile, 'r') as f:
+        print csvfile
+        r = csv.reader(f)
+        for (row_index, row) in enumerate(r):
+             for (col_index, data) in enumerate(row):
+                worksheet.write(row_index, col_index, data)
+                worksheet.set_column(0, col_index, 100)
+        workbook.close()
