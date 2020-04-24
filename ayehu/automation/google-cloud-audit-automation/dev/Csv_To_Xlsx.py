7ef3508be7ee29a91aeb9974349613696452d652
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2020-04-24 15:11:38 +0530

    Update and rename test.py to Csv_To_Xlsx.py

---
+++ ayehu/automation/google-cloud-audit-automation/dev/Csv_To_Xlsx.py
@@ -0,0 +1,15 @@
+import csv, os
+from glob import glob
+from xlsxwriter.workbook import Workbook
+for csvfile in glob('./*.csv'):
+  name = os.path.basename(csvfile).split('.')[-2]
+workbook = Workbook('./' + str(name) + '.xlsx', {'strings_to_numbers': True,
+'constant_memory': True})
+worksheet = workbook.add_worksheet()
+with open(csvfile, 'r') as f:
+ r = csv.reader(f)
+ for row_index, row in enumerate(r):
+   for col_index, data in enumerate(row):
+     worksheet.write(row_index, col_index, data)
+     worksheet.set_column(0, col_index, 100)
+workbook.close()
