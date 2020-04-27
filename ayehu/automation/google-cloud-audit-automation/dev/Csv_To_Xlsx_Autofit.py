8d4958a68f8383d018d2bdc6564b6bbdf76c6281
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2020-04-27 20:32:32 +0530

    Create Csv_To_Xlsx_Autofit.py

---
+++ ayehu/automation/google-cloud-audit-automation/dev/Csv_To_Xlsx_Autofit.py
@@ -0,0 +1,40 @@
+def csv_to_excel():
+    import csv
+    import os
+    from glob import glob
+    from xlsxwriter.workbook import Workbook
+    for csvfile in glob('./*.csv'):
+        name = os.path.basename(csvfile).split('.')[-2]
+        workbook = Workbook('./' + str(name) + '.xlsx',
+                            {'strings_to_numbers': True,
+                            'constant_memory': True})
+        worksheet = workbook.add_worksheet()
+        with open(csvfile, 'r') as f:
+            r = csv.reader(f)
+            for (row_index, row) in enumerate(r):
+                for (col_index, data) in enumerate(row):
+                    worksheet.write(row_index, col_index, data)
+            workbook.close()
+
+def Excel_autofit():
+    import openpyxl
+    from string import ascii_uppercase
+    from glob import glob
+    for file in glob('./*.xlsx'):
+        wb = openpyxl.load_workbook(filename=file)
+        worksheet = wb.active
+        for col in worksheet.columns:
+          max_length = 0
+          column = col[0].column
+          for cell in col:
+            try:  # Necessary to avoid error on empty cells
+              if len(str(cell.value)) > max_length:
+                max_length = len(cell.value)
+            except:
+              pass
+          adjusted_width = max_length
+          worksheet.column_dimensions[column].width = adjusted_width
+        wb.save(file)
+
+csv_to_excel()
+Excel_autofit()
